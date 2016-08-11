a7e861aa4451e64367c2941034ed70f1b24d0467
Karola <yumi.chan@interia.pl>
2016-08-11 14:48:57 +0200

    list added to fragrance controller

--- src/main/java/it/morfoza/karo/perfume/FragranceController.java
+++ src/main/java/it/morfoza/karo/perfume/FragranceController.java
@@ -5,9 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class FragranceController {
 
+    private List<Fragrance> allPerfume = new ArrayList<>();
+
+    public FragranceController() {
+        allPerfume.add(new Fragrance("Calvin Kleine Euphoria", "Głowa- zielona nuta, owoc granatu, kaki Serce - lotos, orchidea, Podstawa - piżmo, mahoń, nuty śmietany, bursztyn, fiołek"));
+        allPerfume.add(new Fragrance("Calvin Kleine IN2U", "Głowa - różowy grapefruit, liście czarnej porzeczki, bergamotka sycylijska, Serce - kaktus, orchidea, Podstawa - cedr, bursztyn, wanilia"));
+        allPerfume.add(new Fragrance("DKNY Be Delicious", "Głowa - grejpfrut, ogórek, magnolia, Serce - róża, konwalia, tuberoza, fiołek, zielone jabłko, Podstawa - bursztyn, drzewo sandałowe, nuty drzew"));
+        allPerfume.add(new Fragrance("Giorgio Armani", "Głowa - liście czarnej porzeczki, Serce - frezja, róża, Podstawa - wanilia, ambroxan, paczuli, nuty drzew"));
+        allPerfume.add(new Fragrance("Calvin Kleine Summer", "Głowa - cytryna, mojito, zielone jabłko, imbir, limonka, Serce - jałowiec, Tahitiańska gardenia, guajawa, Podstawa - mleczko kokosowe, cedr, mech"));
+    }
 
     @RequestMapping("/")
     public String fragrance() { return "fragrance"; }
@@ -18,8 +30,14 @@ public class FragranceController {
             @RequestParam(value = "search") String search,
             @RequestParam(value = "operation", required = false, defaultValue = "search") String operation, Model model) {
 
-        String result ="Tu byłam! Karolina!";
-        model.addAttribute("resultOfOperation", result);
+        List<Fragrance> allMatchPerfume = new ArrayList<>();
+        for (Fragrance fragrance: allPerfume) {
+            if (fragrance.name.contains(search) || fragrance.ingredients.contains(search)) {
+                allMatchPerfume.add(fragrance);
+            }
+        }
+
+        model.addAttribute("resultOfOperation", allMatchPerfume);
         return "result";
 
     }
